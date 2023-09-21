99c025cdeb9a18c67ea4266e52e1fe83303d6ed8
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-21 13:05:13 +0200

    add receivings

--- src/org/lessons/java/wish/Main.java
+++ src/org/lessons/java/wish/Main.java
@@ -1,31 +1,45 @@
 package org.lessons.java.wish;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 	
-	List<String> wishList = new ArrayList<>();
-
+	//List<String> wishList = new ArrayList<>();
+	Map<String, String> wishList = new HashMap<>();
+	
 	boolean stop = false;
 	Scanner sc = new Scanner(System.in);
+	System.out.println("WISH LIST: (insert 'stop' to end)");
 	while(!stop)
 	{
+		System.out.println("Who’s the gift for?");
 		
-		System.out.println("Insert a gift name. Insert 'stop' to stop");
-		
+		String receiving = sc.nextLine();
+		System.out.println("Add a gift");
 		String gift = sc.nextLine();
-		if (gift.equals("stop")) {
+		
+		if(receiving.equals("stop")|| gift.equals("stop")) {
 			stop = true;
 		} else {
-			wishList.add(gift);
-			System.out.println("You have " + wishList.size() + " gifts" );
+			wishList.put(receiving, gift);
+			System.out.println("You have " + wishList.size() + " gifts");
 		}
+//		System.out.println("Insert a gift name. Insert 'stop' to stop");
+//		
+//		String gift = sc.nextLine();
+//		if (gift.equals("stop")) {
+//			stop = true;
+//		} else {
+//			wishList.add(gift);
+//			System.out.println("You have " + wishList.size() + " gifts" );
+//		}
+//		
 		
-		
-			
 	}
 	System.out.println(wishList);
 	sc.close();
