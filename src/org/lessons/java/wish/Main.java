0339c34d939723d050c1b9a43c945d39ef39ff6f
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-21 12:53:36 +0200

    add wish list

--- src/org/lessons/java/wish/Main.java
+++ src/org/lessons/java/wish/Main.java
@@ -1,5 +1,35 @@
 package org.lessons.java.wish;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+	public static void main(String[] args) {
+	
+	List<String> wishList = new ArrayList<>();
+
+	boolean stop = false;
+	Scanner sc = new Scanner(System.in);
+	while(!stop)
+	{
+		
+		System.out.println("Insert a gift name. Insert 'stop' to stop");
+		
+		String gift = sc.nextLine();
+		if (gift.equals("stop")) {
+			stop = true;
+		} else {
+			wishList.add(gift);
+			System.out.println("You have " + wishList.size() + " gifts" );
+		}
+		
+		
+			
+	}
+	System.out.println(wishList);
+	sc.close();
+	
+}
 
 }
